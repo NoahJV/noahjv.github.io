8963bc3a9a9fded75326dc57e66f6e656509a5d3
NoahJV <noahverburg2907@gmail.com>
2025-09-09 14:48:52 +0200

    Attempted fix for blank page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-website/',
+  base: '/noahjv.github.io/',
   plugins: [react(), tailwindcss()],
   resolve: { 
     alias: { 
